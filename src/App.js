12dce63644f6e057d0fc3f2f0b8b40b2d134f2a5
bslzz <bishalkoid@gmail.com>
2020-12-13 11:32:11 +0200

    initial start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
-import Contacts from './components/contacts/Contacts';
-import Navbar from './components/layout/Navbar';
-import './styles/App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <div className="py-3">
-          <Contacts />
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import store from './store';
-import { Provider } from 'react-redux';
-
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
